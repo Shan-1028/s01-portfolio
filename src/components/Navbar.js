3174409b12db1d7a7fa0d7e87ad612828a1ec6a7
Shiela Rose Marilao <shielarosemarilao@gmail.com>
2022-02-22 19:22:51 +0800

    Add feature branch and update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,13 @@ import Nav from 'react-bootstrap/Nav'
 
 export default function AppNavbar() {
     return (
-        <Navbar bg="light" expand="lg">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar bg="warning" expand="lg">
+        <Navbar.Brand href="#home">Gitlab Merge Request</Navbar.Brand>
             <Nav className="mr-auto">
                 <Nav.Link href="#home">Home</Nav.Link>
                 <Nav.Link href="#link">Link</Nav.Link>
+                <Nav.Link href="#link">About Us</Nav.Link>
+                <Nav.Link href="#link">Projects</Nav.Link>
             </Nav>
         </Navbar>
     )
